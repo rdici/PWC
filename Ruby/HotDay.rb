875c285940c2ea74b7da9c9cd28e32eefa9c02a2
rdici <rdici@yahoo.com>
2022-09-08 09:26:40 -0500

    Challenge 181

---
+++ Ruby/HotDay.rb
@@ -0,0 +1,46 @@
+#!ruby.exe
+require 'date'
+
+# AUTHOR: Robert DiCicco
+# DATE: 2022-09-08
+# Challenge #181 Hot Day ( Ruby )
+
+$daytemps = Hash.new
+
+def compareTemps
+  $daytemps.each do |mydate, mytemp|
+    d = Date.parse(mydate)
+    d = d - 1
+    prevday = d.strftime("%Y-%m-%d")
+    if ! $daytemps.has_key?(prevday)
+      next
+    end
+    if $daytemps[mydate] > $daytemps[prevday]
+      puts(mydate)
+    end
+  end
+end
+
+def setupHashArray 
+  File.foreach("temperature.txt") { |line| 
+    line = line.gsub(/[[:space:]]/, '')
+    (mydate, mytemp) = line.split(',')
+    $daytemps[mydate.chomp] = mytemp.chomp
+  }
+end
+
+def main
+  setupHashArray()
+  compareTemps()
+end
+
+main()
+
+#----------------------------------------------
+# SAMPLE OUTPUT
+# PS G:\Projects\Perl\Challenges> ruby .\HotDay.rb
+# 2022-08-06
+# 2022-08-05
+# 2022-08-10
+# 2022-08-08
+# 2022-08-02

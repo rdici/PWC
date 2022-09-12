240efb7b786e36428e05d7700539ae6ff284fe91
rdici <rdici@yahoo.com>
2022-09-12 07:38:56 -0500

    Challenge 182

---
+++ Perl/MaxIndex.pl
@@ -0,0 +1,35 @@
+#!perl.exe
+use strict;
+use warnings;
+use feature qw/say/;
+
+# AUTHOR: Robert DiCicco
+# DATE: 2022-09-12
+# Challenge 182 Max Index ch-1.pl ( Perl )
+
+my @n = (5, 2, 9, 1, 7, 6);
+
+my %n_ndx = ();
+my $count = 0;
+
+foreach (@n) {
+  $n_ndx{$_} = $count++;
+}
+
+my @n_srt = reverse sort(@n);
+
+print "Input: \@n = \(@n\)\n";
+print "Output: $n_ndx{$n_srt[0]}\n";
+
+#---------------------------------------------
+# SAMPLE OUTPUT
+# PS G:\Projects\Perl\Challenges> perl .\MaxIndex.pl
+# Input: @n = (5 2 9 1 7 6)
+# Output: 2
+
+# PS G:\Projects\Perl\Challenges> perl .\MaxIndex.pl
+# Input: @n = (5 2 6 8 1 7 6)
+# Output: 3
+
+
+

89dd267ba585368e09f8d097d4d56996fd4d9b2e
rdici <rdici@yahoo.com>
2022-10-03 07:18:50 -0500

    Challenge 185

---
+++ Perl/MacAddress.pl
@@ -0,0 +1,29 @@
+#!perl.exe
+# AUTHOR: Robert DiCicco
+# DATE: 2022-10-03
+# Challenge 185  Mac Address ( Perl )
+
+use strict;
+use warnings;
+use feature "say";
+
+my @inp = ("1ac2.34f0.b1c2", "abc1.20f1.345a");
+
+
+for (@inp) {
+  my $inp_numalpha = (join '', split '\.', $_);     # remove periods from string
+  my $x = 0;
+  my $outstr = '';
+  while($x < length($inp_numalpha)) {
+    $outstr .= substr($inp_numalpha,$x,2).":";      # print groups of two with ":"
+    $x += 2;                                        # bump offset by 2
+  }
+  chop($outstr);                                    # remove trailing ":"
+  say $outstr;
+}
+
+#--------------------------------------------------
+# SAMPLE OUTPUT
+# PS G:\Projects\Perl\Challenges> perl .\MacAddress.pl
+# 1a:c2:34:f0:b1:c2
+# ab:c1:20:f1:34:5a

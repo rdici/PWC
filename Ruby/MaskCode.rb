62cd50a3a0d2e02ab1f4ddb87e41711ad848ce71
rdici <rdici@yahoo.com>
2022-10-04 09:15:33 -0500

    Challenge 185

---
+++ Ruby/MaskCode.rb
@@ -0,0 +1,29 @@
+#!ruby.exe
+# AUTHOR: Robert DiCicco
+# DATE: 2022-10-04
+# Challenge 185 Mask Code ( Ruby )
+
+inp = ['ab-cde-123', '123.abc.420', '3abc-0010.xy', '1234567.a', 'a-1234-bc', 'a.b.c.d.e.f']
+
+for code in inp do
+  cnt = 0;                    # where we are in the string
+  fnd = 0;                    # count of characters changed
+  while fnd < 4 do
+    if (code[cnt,1] =~ /[0-9]|[a-z]/)
+     code[cnt] = 'x'
+     fnd += 1
+    end
+    cnt += 1
+  end
+  puts code
+end     
+
+#----------------------------------------
+# SAMPLE OUTPUT
+# PS G:\Projects\Perl\Challenges> ruby .\MaskCode.rb
+# xx-xxe-123
+# xxx.xbc.420
+# xxxx-0010.xy
+# xxxx567.a
+# x-xxx4-bc
+# x.x.x.x.e.f

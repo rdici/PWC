9baa22b633b47735546b4d2714e2545b60ed86ba
rdici <rdici@yahoo.com>
2022-10-05 10:56:59 -0500

    Challenge 185 fully commented version

--- Julia/MaskCode.jl
+++ Julia/MaskCode.jl
@@ -6,19 +6,19 @@
 inp = ["ab-cde-123", "123.abc.420", "3abc-0010.xy", "1234567.a", "a-1234-bc", "a.b.c.d.e.f"]
 
 for i in inp 
-  cnt = 1;                    # where we are in the string
+  cnt = 1;                    # index counter, julia indices start at 1
   fnd = 0;                    # count of characters changed
   while fnd < 4 
     sp = SubString(i,cnt,cnt)
-    if ! occursin(r"[a-z0-9]", sp)
-      cnt += 1
-      continue
+    if ! occursin(r"[a-z0-9]", sp)    # if not alpha or number digit
+      cnt += 1                        # bump index counter
+      continue                        # and continue loop
     end
-    i_arr = split(i,"")
-    i_arr[cnt] = "x"
-    i = join(i_arr)
-    fnd += 1
-    cnt += 1
+    i_arr = split(i,"")               # split the string to an array
+    i_arr[cnt] = "x"                  # change the char to an 'x'
+    i = join(i_arr)                   # join the array back to a string
+    fnd += 1                          # bump the found counter
+    cnt += 1                          # bump index counter
   end
   println(i)
 end

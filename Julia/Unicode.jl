742f71a1ca99f37213e83676cc2804ebc41d6bcf
rdici <rdici@yahoo.com>
2022-10-10 16:38:50 -0500

    Challenge 186

---
+++ Julia/Unicode.jl
@@ -0,0 +1,18 @@
+#!/usr/bin/env julia
+using Unicode
+
+# AUTHOR: Robert DiCicco
+# DATE: 2022-10-10
+# Challenge 186 Unicode Makeover ( Julia )
+
+myarr = ["âÊíÒÙ", "ÃÊÍÒÙ"]
+
+for mystr in myarr
+  println(Unicode.normalize(mystr, stripmark=true))
+end
+
+#-----------------------------
+# SAMPLE OUTPUT
+# PS G:\Projects\Perl\Challenges> julia .\Unicode.jl
+# aEiOU
+# AEIOU

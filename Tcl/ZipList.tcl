4f2cbe46634672f578cdf3c4e7d5a677dae6c951
rdici <rdici@yahoo.com>
2022-10-13 05:47:10 -0500

    Challenge 186 chged to use string length

--- Tcl/ZipList.tcl
+++ Tcl/ZipList.tcl
@@ -7,7 +7,7 @@ set a {abc}
 set b {123}
 
 proc zip {arr1 arr2} {
-  for {set i 0} {$i < 3} {incr i} {
+  for {set i 0} {$i < [string length $arr1]} {incr i} {
     set first [string range $arr1 $i $i]
     set second [string range $arr2 $i $i]
     puts -nonewline "$first $second "

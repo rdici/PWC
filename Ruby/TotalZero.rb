8c033e60bd11f9ef8b37cc9d2e7b19cbb483f3b2
rdici <rdici@yahoo.com>
2022-10-25 15:38:32 -0500

    Challenge 188

---
+++ Ruby/TotalZero.rb
@@ -0,0 +1,65 @@
+#!/usr/bin/env ruby
+=begin
+AUTHOR: Robert DiCicco
+DATE: 2022-10-25
+Challenge 188 Total Zero ( Ruby )
+
+You are given two positive integers $x and $y.
+Write a script to find out the number of operations needed to make both ZERO. Each operation is made up either of the followings:
+
+$x = $x - $y if $x >= $y
+or
+$y = $y - $x if $y >= $x (using the original value of $x)
+=end
+
+$step = 0
+
+def steps ( x, y )
+  if ( x > y ) then
+    x = x - y
+  elsif (y > x ) then
+    y = y - x
+  end
+  $step += 1
+  if ( x == y) then
+    $step += 1
+    puts "Output: #{$step}";;
+     puts " "
+    return;  
+  end
+  steps(x,y)
+end
+
+def main()
+  g = [[5,4], [4,6], [2,5], [3,1], [7,4]];
+  g.each do |f|
+    x = f[0]
+    y = f[1]
+    puts("Input: $x = #{x},  $y = #{y}");
+    $step = 0;
+    steps(x,y)
+  end
+end
+
+main()
+
+=begin
+------------------------------------------------
+SAMPLE OUTPUT
+Input: $x = 5,  $y = 4
+Output: 5
+
+Input: $x = 4,  $y = 6
+Output: 3
+
+Input: $x = 2,  $y = 5
+Output: 4
+
+Input: $x = 3,  $y = 1
+Output: 3
+
+Input: $x = 7,  $y = 4
+Output: 5
+
+=end
+  

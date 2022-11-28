c07ab529b1687a0a8fd857e581b25f87fdfbe9cf
rdici <rdici@yahoo.com>
2022-11-28 13:27:24 -0600

    Challenge 193 - hardcoded array

--- Python/BinaryString.py
+++ Python/BinaryString.py
@@ -27,9 +27,8 @@ Input: $n =  4
 Output:  0000 0001 0010 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 1101 1110 1111
 '''
 
-arr = [2,3,4]
 
-for p in arr:
+for p in [2,3,4]:
   print("Input: $n = ",p)
   rng = range(2**p)
   print("Output: ", end=" ")

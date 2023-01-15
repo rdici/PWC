a25a8e81eabc4602483adfeca80be6072833c32e
rdici <rdici@yahoo.com>
2023-01-15 15:28:56 -0600

    Challenge 199 Rework

--- Julia/GoodTriplets.jl
+++ Julia/GoodTriplets.jl
@@ -1,7 +1,7 @@
 #!/usr/bin/env julia
 #=
 AUTHOR: Robert DiCicco
-DATE  : 2023-01-13
+DATE  : 2023-01-15
 Challenge 199 Good Triplets ( Julia )
 =#
 using Combinatorics
@@ -14,31 +14,21 @@ z = 3
 seen = Dict()
 
 for zz in combinations(list,3)
-	if ! haskey(seen,zz)
+	if haskey(seen,zz)
+		println(zz)
+	else
+		x1 = findfirst(item -> item == zz[1], list)
+		x2 = findfirst(item -> item == zz[2], list)
+		x3 = findfirst(item -> item == zz[3], list)
+		if x1 > x2 || x2 > x3 || x1 > x3
+			continue
+		else
+			if abs(zz[1] - zz[2]) > x || abs(zz[2] - zz[3]) > y || abs(zz[1] - zz[3]) > z
+				continue
+			else
+				println(zz)
+			end
+		end
 		seen[zz] = 1
 	end
 end
-
-for ( key,_ ) in seen
-   	x1 = findfirst(item -> item == key[1], list)
-	x2 = findfirst(item -> item == key[2], list)
-	x3 = findfirst(item -> item == key[3], list)
-	if x1 > x2 || x2 > x3 || x1 > x3
-		continue
-	else
-		if abs(key[1] - key[2]) > x || abs(key[2] - key[3]) > y || abs(key[1] - key[3]) > z
-			#println(key)
-		else
-			println(key)
-		end
-	end
-end
-
-#=
-SAMPLE OUTPUT
-julia .\GoodTriplets.jl
-[3, 0, 1]
-[3, 1, 1]
-[0, 1, 1]
-=#
-

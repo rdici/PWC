32f7eb7c375a15fca07efdbc577b3c8b0333b053
rdici <rdici@yahoo.com>
2023-03-04 09:45:46 -0600

    Initial Rust

---
+++ Rust/shortest_times/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let myarr  = [[1,2,3], [1,9,6]];
+    for i in 0..myarr.len() {
+        let a1 = myarr[i];
+        //print!("Input: @array = {:?}\n",myarr[i]);
+        print!("{:?}\n",a1);
+    }
+}

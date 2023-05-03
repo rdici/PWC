40124cbc2954a07d3af30c194ea994e6c5e66bf8
rdici <rdici@yahoo.com>
2023-05-03 14:07:31 -0500

    Challenge 215

---
+++ Julia/NumberPlacement.jl
@@ -0,0 +1,49 @@
+#!/usr/bin/env julia
+#=
+-----------------------------------------
+AUTHOR: Robert DiCicco
+DATE  : 2023-05-03
+Challenge 215 Number Placement ( Ruby )
+-----------------------------------------
+=#
+using Printf
+
+numbers = [[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,0,0,0,0,1]];
+counts  = [1,2,3]
+
+cnt=1
+
+function CheckZeroes(arr) 
+    zcnt = 1;
+    while zcnt < length(arr) 
+        if ((arr[zcnt] == 0) && (arr[zcnt-1] == 0)) 
+             arr[zcnt] = 1;
+        end
+        zcnt += 1;
+    end
+    @printf("Output: 1 = %s\n\n",arr)
+end
+
+function HowManyZeroes(arr) 
+    z = 1
+    zcnt = 0
+    while z < (length(arr)) 
+        if (arr[z] == 0) 
+            zcnt += 1
+        end
+        z += 1
+    end
+    return zcnt
+end
+
+for nums in numbers
+    global cnt
+    @printf("Input: @numbers = %s  Count = %d\n",nums,counts[cnt])
+    zeroes = HowManyZeroes(nums)
+    if (zeroes < cnt * 2) 
+        @printf("Output: 0\n\n")
+    else 
+        CheckZeroes(nums)
+    end
+    cnt += 1
+end
